4fae5718c5c6327f01444151a823bdb3b38f07ee
Kabilan Jayabalan <kabilan2k06@gmail.com>
2026-08-12 23:08:22 +0530

    Sort Array By Parity

---
+++ Arrays/Sort Array By Parity.java
@@ -0,0 +1,19 @@
+class Solution { //Leetcode : 905
+    public int[] sortArrayByParity(int[] nums) {
+        int[] arr = new int[nums.length];
+        int j = 0;
+        int k = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                arr[j] = nums[i];
+                j++;
+            } else {
+                arr[k] = nums[i];
+                k--;
+            }
+        }
+
+        return arr;
+    }
+}
